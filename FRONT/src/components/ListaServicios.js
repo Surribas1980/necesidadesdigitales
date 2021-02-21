b4fcdd981bd22b8b57547c3a5fa0a94c59147348
= <=>
2021-02-21 09:37:30 +0100

    Quitando cosillas y poniendo -borrar- en el boton

--- FRONT/src/components/ListaServicios.js
+++ FRONT/src/components/ListaServicios.js
@@ -55,17 +55,13 @@ export default function ListaServicios(props){
     //<>{props.servicios.map((servicio)=>{return <p>servicio.titulo_ser</p>})}</>
     return (<><h1>Lista de servicios</h1> 
     
-    <ul>
-
-        {props.valores.map((servicio)=>{return <li key={servicio.id_ser}>{servicio.titulo_ser}</li>})}
-        </ul>
 <form onSubmit={handleSubmit(onSubmit)}>
     {props.valores.map((item) => {
         const fieldName = `servicio[${item}]`;
         return (
             <fieldset name={fieldName} key={item.id_ser}>
                 <label>
-                    {item.expli_ser}
+                    Titulo: {item.titulo_ser}<br></br>Explicación:{item.expli_ser}
                     <input type="checkbox" value={item.id_ser} name={`${fieldName}.expli_ser`} ref={register} />
                 </label>
             </fieldset>
@@ -73,7 +69,7 @@ export default function ListaServicios(props){
     })
 
     }
-    <button>Enviar</button>
+    <button>Borrar</button>
 </form>
       
         </>
