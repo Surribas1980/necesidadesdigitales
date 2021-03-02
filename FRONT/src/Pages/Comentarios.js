c1a210ab9a5df29424fac5606bc1bd4668335a62
= <=>
2021-03-02 20:41:23 +0100

    Despues de modificar en el back no vale el componente desarrollado...,puse el bisjejeje y ok

--- FRONT/src/Pages/Comentarios.js
+++ FRONT/src/Pages/Comentarios.js
@@ -1,5 +1,6 @@
 import { useState,useEffect} from 'react';
 import ComentariosLista from '../components/User/ComentariosLista';
+import ServiciosSolucionados from '../components/User/ServiciosSolucionados';
 import { deleteService } from '../http/api';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import InsertComentarios from './InsertComentarios';
@@ -11,21 +12,29 @@ export default function Comentarios(){
         const comentServicios = async () => {
             const data = await deleteService("/comentar",'GET',0,0);
             console.log('datos : ', data);
-            setComentarios(data);
+            
+            
+            setComentarios(data['data']);
         }
         comentServicios();
     },[]);
-
+    
+    //<ComentariosLista valores={comentarios}></ComentariosLista>
     return (<>
         <h1>Estoy en comentarios</h1>
             <Router>
                 <ul>
                     <Link to="/nada">Recoger en Comentarios</Link>
-                    <li><Link to="/insertarcomentario">Hacer Comentario</Link></li>
+                    <li><Link to="/insertarcomentario">Conversaciones</Link></li>
+                    <li><Link to="/comentarios" >Mis conversaciones</Link></li>
                 </ul>
 
                 <Route path="/insertarcomentario">
                     <InsertComentarios />
+                    <ServiciosSolucionados solucionados={comentarios}></ServiciosSolucionados>
+                </Route>
+                <Route path="/comentarios">
+
                 </Route>
                 <Route path="/nada">
 
@@ -33,6 +42,5 @@ export default function Comentarios(){
 
             </Router>
 
-        <ComentariosLista valores={comentarios}></ComentariosLista>
         </>);
 }
