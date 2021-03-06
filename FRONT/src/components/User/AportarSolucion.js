23355ecbba18eaef277cf957606594e4fa857047
= <=>
2021-03-06 09:08:59 +0100

    con el id en el formulario en oculto

--- FRONT/src/components/User/AportarSolucion.js
+++ FRONT/src/components/User/AportarSolucion.js
@@ -4,7 +4,8 @@ import {useState} from 'react';
 
 export default function AportarSolucion(props){
     const {register, handleSubmit} = useForm();
-    const {id,setId}=useState(0);
+    //const {id,setId}=useState(0);
+    let id= props.id;
     //setId(props.id);
 
     const onSubmit = (data)=>{
@@ -13,8 +14,10 @@ export default function AportarSolucion(props){
 
     return (<>
     <form onSubmit={onSubmit(handleSubmit)}>
+        <br></br>Id:{id}<br></br>
         <label htmlFor="ficheros">Inserte los ficheros</label>
-        <input id="ficheros" ref={register({required: true})} name="ficheros" multiple type="file"></input>
+        <input id="ficheros" ref={register({required: true})} name="ficheros" multiple type="file"></input><br></br>
+        Entrada: <input id="id_ser" ref={register({required: false})} name="id_ser" type="hidden" value={id}></input>
     </form>
     </>);
 }
