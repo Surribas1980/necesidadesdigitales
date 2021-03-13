4018dbd7e918445f609a53b955b942168d6c74c4
= <=>
2021-03-13 21:38:50 +0100

    Empezando con dar puntuacion

--- FRONT/src/components/User/DarPuntuacion.js
+++ FRONT/src/components/User/DarPuntuacion.js
@@ -1,13 +1,49 @@
+import { useState,useEffect} from 'react';
+import {deleteService} from '../../http/api';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import '../../css/DarPuntuacion.css';
+import MisServNoSol from './MisServNoSol';
+import MisSolDesc from './MisSolDesc';
 export default function DarPuntuacion(){ 
-    let ventana;
-    let abrirVentana = (idServicio)=>{
-        let configuracion_ventana = "height=300,width=300,menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
-        const dir = "C:\\Users\\Israel\\Documents\\HACKBOSS\\Trabajo\\Necesidades_digitales\\ProyectoNecesidadesDigitales\\BACK\\docs\\servicios\\solucionados\\";
-        window.open(dir,"myWindow",
-        configuracion_ventana);
+    const [misServNoSol,setmisSerNoSol] = useState([]);
+    const [misSolDesc, setmisSolDesc] = useState([]);
+    const [showMenu, setShowMenu]=useState(false);
+    useEffect(()=>{
+        const pedirmisServicios = async ()=>{
+
+            const misServicios = await deleteService('misservicios','GET',0,0);
+            console.log('misServicios ',misServicios);
+            setmisSerNoSol(misServicios['misServiciosNoSolucionadosConSolucionadores'][0])
+            setmisSolDesc(misServicios['misSolucionesDescartadas'][0]);
+
+        }
+
+        pedirmisServicios();
     }
+    ,[]);
+    
 
     return(<><h1>Estoy en dar puntuación</h1>
-    <button >Ver solución</button>
+    <Router>
+
+    <button onClick={()=>{setShowMenu(!showMenu)}}>Ver menús</button>
+    {
+        showMenu &&
+            <nav>
+                <div className="header-item1">
+                    <Link to ="misservisnosol">Posibles soluciones</Link>
+                </div>
+                <div className="header-item1">
+                    <Link to = "missolucionesdescartadas">Mis soluciones descartadas</Link>
+                </div>
+            </nav>
+    }
+        <Route path="misservisnosol">
+            <MisSolDesc nosolucionados={misServNoSol}></MisSolDesc>
+        </Route>
+        <Route path="missolucionesdescartadas">
+            <MisServNoSol descartadas={misSolDesc}></MisServNoSol>
+        </Route>
+    </Router>
     </>);
 }
---
+++ FRONT/src/components/User/MisSolDesc.js
@@ -0,0 +1,7 @@
+export default function MisSolDesc(props){
+    const servicios = props.nosolucionados;
+
+    return (<><h3>Posibles soluciones</h3>
+    </>);
+
+}
