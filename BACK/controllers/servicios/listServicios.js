17f78ee4ee6e874ff9de430d324649a780511bba
= <=>
2021-03-27 09:09:43 +0100

    vou remar

--- BACK/controllers/servicios/listServicios.js
+++ BACK/controllers/servicios/listServicios.js
@@ -8,31 +8,37 @@ const listServicios = async (req, res, next) => {
     let results;
     //Saco queryString
     const { limite,inicioLista,alante,search1,search2,mi } = req.query;
-    
+    console.log('entrando en listServicios',limite,inicioLista,alante,search1,search2,mi)
     if(search1 || search2){
       //ojo, esto puede dar lugar a error si desde el front no se hace la búsquda de manera
       //correcta
-        await connection.query(`call tablaLimitadaServicios(?, ?, ?, ?, ?);`,[mi,req.userAuth.id,limite,inicioLista,alante]);
+      await connection.query(`call tablaLimitadaServicios(?, ?, ?, ?, ?);`,[mi,req.userAuth.id,limite,inicioLista,alante]);
         [results]= await connection.query(`call buscarValor(?,?);`,[search1,search2]);
     }else{
             if(uno === alante){
               await connection.query(`call tablaLimitadaServicios(?, ?, ?, ?, ?);`,[mi,req.userAuth.id,limite,inicioLista,alante]);
             }else{
+              console.log('Vou para atrás')
               await connection.query(`call miBucle(?, ?);`,[limite,inicioLista]);
             }
             
            [results] = await connection.query(`select * from servicioslimitada;`);
             
     }
-          const [numServicios] = await connection.query(`select count(*) from servicios join solicitar on id_ser = id_ser_soli where id_usu_soli = ? `,[req.userAuth.id]);
+          const [idMax] = await connection.query(`select max(id_ser) from servicios join solicitar on id_ser = id_ser_soli where id_usu_soli = ? `,[req.userAuth.id]);
+          const [idMin] = await connection.query(`select min(id_ser) from servicios join solicitar on id_ser = id_ser_soli where id_usu_soli = ? `,[req.userAuth.id]);
           const [idMaxTemporal] = await connection.query(`select idMaxServiciosTemporal();`);
           const [idMinTemporal] = await connection.query(`select idMinServiciosTemporal();`);
+          const [misServicios] = await connection.query(`select * from servicios join solicitar on id_ser = id_ser_soli where id_usu_soli = ? `,[req.userAuth.id]);
+       
           await connection.query(`call borrarTemporalServicios();`);
           
     //Devuelto un json con los servicios
     res.send({
       status: "ok",
-      numservicios:numServicios,
+      misServicios:misServicios,
+      idMin:idMin,
+      idMax:idMax,
       data: limite,
       inicioLista,
       resultbbdd: results,
