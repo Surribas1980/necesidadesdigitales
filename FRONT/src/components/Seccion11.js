40b4014d418ad03b0063a4d1da54263882df1ee8
= <=>
2021-05-06 19:20:55 +0200

    avanzando ..

--- FRONT/src/components/Seccion11.js
+++ FRONT/src/components/Seccion11.js
@@ -10,34 +10,42 @@ function Seccion11() {
         
            
             <div className="seccion11">
-
-            Lorem ipsum dolor sit amet, ea eum labitur scsstie percipitoleat fabulas complectitur deterruisset at pro. Odio quaeque reformidans est eu, expetendis intellegebat has ut, viderer invenire ut his. Has molestie percipit an. Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea. No soleat fabulas prodesset vel, ut quo solum dicunt. Nec et jority have suffered alteration.
-
-Odio quaeque reformidans est eu, expetendis intellegebat has ut, viderer invenire ut his. Has molestie percipit an. Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea. No soleat fabulas prodesset vel, ut quo solum dicunt. Nec et amet vidisse mentitumsstie percipitoleat fabulas. 
-            </div>
-        
-            <div className="seccion22">
-                <div className="icono">
-                    <FontAwesomeIcon icon={faHeadSideVirus}></FontAwesomeIcon><FontAwesomeIcon icon={faComment}></FontAwesomeIcon>
-                </div>
+                <h3>Titulo</h3>
+                <div className="despues">Lorem ipsum dolor sit amet, ea eum labitur scsstie percipitoleat fabulas complectitur deterruisset at pro. </div>
+                <div className="despues">Intellegebat has ut. Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea. No soleat fabulas prodesset vel, ut quo solum dicunt. Nec et jority have suffered alteration.</div>
+                <div className="despues">Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea. No soleat fabulas prodesset vel, ut quo solum dicunt. Nec et amet vidisse mentitumsstie percipitoleat fabulas. </div>
                 
 
-                <img className="laimg" src={`http://localhost:4000/imagenes/imagenes/1640.png`} alt="imagen"/>
+
+            </div>
+            <div className="seccion22">
+                <h3>Titulo 3</h3>
+                <section>
+                    <article>
+
+                    Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a
+                    </article>
+                    <article>
+
+                    <img className="laimg" src={`http://localhost:4000/imagenes/imagenes/1640.png`} alt="imagen"/>
+                    </article>
+                </section>
+
                 
             
-                <div className="eltex">
+                {/*<div className="eltex">
                     Podrás interactuar con otras personas
                     
 
-                </div>
+                </div>*/}
             </div>
             <div className="seccion33">
-                <div className="icono">
-                    <FontAwesomeIcon icon={faUniversity}></FontAwesomeIcon>
+                <h3>Titulo ...</h3>
+                <div className="despues">
+                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a
                 </div>
-            
-                <div>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a
+                <div className="centrado">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
                 </div>
             </div>
     </div>
